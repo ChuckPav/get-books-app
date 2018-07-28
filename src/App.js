b3d2804f6d29e428dcc8140192726863afe88ad9
Charles <cpavlick@redventures.com>
2018-07-28 09:00:55 -0400

    MVP finished

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ class App extends Component {
     super();
 
     this.state = {
-      books: []
-    }
+      books: [],
+      filterBy: 'none',
+      filterValue: '',
+      sortBy: 'none',
+      limit: 0
+    };
+
+    this.changeFilterBy = this.changeFilterBy.bind(this);
+    this.changeFilterValue = this.changeFilterValue.bind(this);
+    this.changeSortBy = this.changeSortBy.bind(this);
+    this.changeLimit = this.changeLimit.bind(this);
   }
 
   componentDidMount() {
@@ -19,7 +28,24 @@ class App extends Component {
       })
       .then(data => {
         this.setState({books: data});
-      })
+      });
+  }
+
+  changeFilterBy(event) {
+    this.setState({filterBy: event.target.value});
+  }
+
+  changeFilterValue(event) {
+    this.setState({filterValue: event.target.value});
+  }
+
+  changeSortBy(event) {
+    this.setState({sortBy: event.target.value});
+  }
+
+  changeLimit(event) {
+    const limit = parseInt(event.target.value)
+    this.setState({ limit });
   }
 
   render() {
@@ -29,7 +55,51 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Check Out These Books!</h1>
         </header>
-        <BookDisplay filteredBooks={this.state.books} />
+        <div>
+          
+          <p>Filter And Sorting Options:</p>
+          <select id="filterBy" onChange={this.changeFilterBy} value={this.state.filterBy}>
+            <option value="title">Filter By Title</option>
+            <option value="author">Filter By Author</option>
+            <option value="year">Filter By Year</option>
+            <option value="none">No Filter Selected</option>
+          </select>
+          <select id="sortBy" onChange={this.changeSortBy} value={this.state.sortBy}>
+            <option value="titleAsc">Title (Ascending)</option>
+            <option value="titleDesc">Title (Descending)</option>
+            <option value="authorAsc">Author (Ascending)</option>
+            <option value="authorDesc">Author (Descending)</option>
+            <option value="yearAsc">Year (Ascending)</option>
+            <option value="yearDesc">Year (Descending)</option>
+            <option value="none">No Sort Selected</option>
+          </select>
+          {this.state.filterBy !== 'none' &&
+            <div>
+              <p>Filter Term:</p>
+              <input type="text" value={this.state.filterValue} onChange={this.changeFilterValue}/>
+            </div>
+          }
+          { this.state.filterBy === 'none' &&
+            <div>
+              <p>Limit:</p>
+              <select id="limit" onChange={this.changeLimit} value={this.state.limit}>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="0">No Limit</option>
+              </select>
+            </div>
+          }
+        </div>
+        <BookDisplay 
+          books={this.state.books} 
+          filterBy={this.state.filterBy} 
+          filterValue={this.state.filterValue}
+          sortBy={this.state.sortBy}
+          limit={this.state.limit}
+        />
       </div>
     );
   }
--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -1,23 +1,68 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 class BookDisplay extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const bookDisplay = this.props.filteredBooks.map(book => {
-            return (
-              <div id={book.isbn}>
-                <h4>{book.title}</h4>
-                <p>By {book.author}</p>
-                <p>Year {book.year}</p>
-              </div>
-            )
-        });
-
-        return bookDisplay;
+  handleFilter(books, filterTarget, filterValue) {
+    switch (filterTarget) {
+        case 'title':
+          return _.filter(books, (book) => { return book.title.includes(filterValue) });
+        case 'author':
+          return _.filter(books, (book) => { return book.author.includes(filterValue) });
+        case 'year':
+          return _.filter(books, (book) => { return (book.year || "").includes(filterValue) });
+        default:
+          return books;
     }
+  }
+
+  handleSort(books, sortTarget) {
+    switch (sortTarget) {
+        case 'titleAsc':
+          return _.sortBy(books, (book) => { return book.title});
+        case 'titleDesc':
+          return _.sortBy(books, (book) => { return book.title}).reverse();
+        case 'authorAsc':
+          return _.sortBy(books, (book) => { return book.author});
+        case 'authorDesc':
+          return _.sortBy(books, (book) => { return book.author}).reverse();
+        case 'yearAsc':
+          return _.sortBy(books, (book) => { return book.year});
+        case 'yearDesc':
+          return _.sortBy(books, (book) => { return book.year}).reverse();
+        default:
+           return books;
+    }
+  }
+
+  render() {
+    const books = _.get(this.props, 'books');
+    const filterBy = _.get(this.props, 'filterBy');
+    const filterValue = _.get(this.props, 'filterValue');
+    const sortBy = _.get(this.props, 'sortBy');
+    const limit = _.get(this.props, 'limit');
+
+    const filteredBooks = this.handleFilter(books, filterBy, filterValue);
+    const adjustedBooks = this.handleSort(filteredBooks, sortBy);
+    const finalBooks = (limit !== 0)
+        ? adjustedBooks.splice(0, limit) : adjustedBooks;
+    console.log('limit', limit);
+    console.log('finalBooks', finalBooks);
+    const bookList = finalBooks.map(book => {
+      return (
+        <div id={book.isbn}>
+          <h4>{book.title}</h4>
+          <p>By {book.author}</p>
+          <p>Year {book.year}</p>
+        </div>
+      );
+    });
+
+    return bookList;
+  }
 }
 
 export default BookDisplay;
